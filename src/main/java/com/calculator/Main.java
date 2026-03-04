a0ae7fa18bc5472b544a31e1491393a482eb964f
Aditya01237 <pareekaditya01@gmail.com>
2026-03-04 18:36:49 +0530

    updated main file

--- src/main/java/com/calculator/Main.java
+++ src/main/java/com/calculator/Main.java
@@ -5,18 +5,20 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Calculator Service Running...");
+        Calculator calculator = new Calculator();
 
         int a = 5;
         int b = 2;
 
-        System.out.println("Power: " + Math.pow(a,b));
-        System.out.println("Square Root: " + Math.sqrt(a));
+        System.out.println("Power: " + calculator.power(a,b));
+        System.out.println("Square Root: " + calculator.squareRoot(a));
+        System.out.println("Factorial: " + calculator.factorial(a));
+        System.out.println("Natural Log: " + calculator.naturalLog(a));
 
         while(true){
-            try {
+            try{
                 Thread.sleep(10000);
-            } catch(Exception e) {}
+            }catch(Exception e){}
         }
-
     }
 }
