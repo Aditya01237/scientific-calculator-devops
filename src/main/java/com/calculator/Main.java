71c549d1d2e195a05992915de4c86dd396b87029
Aditya01237 <pareekaditya01@gmail.com>
2026-03-06 21:26:35 +0530

    updated deploy.yml

--- src/main/java/com/calculator/Main.java
+++ src/main/java/com/calculator/Main.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Starting Scientific Calculator...");
+        System.out.println("==================================");
+
         Scanner sc = new Scanner(System.in);
         Calculator calc = new Calculator();
         int choice;
